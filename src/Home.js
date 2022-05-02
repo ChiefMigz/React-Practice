1b19bc07e11d837b31ec665eaeda28e0dcd4bcc9
ChiefMigz <pebenitomiguel@gmail.com>
2022-05-02 10:11:27 -0700

    Added blog content for homepage (Part 10)

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,21 @@
 import { useState } from "react"
 
 const Home = () => {
-
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(33);
-
-    const handleClick = () => {
-        setName('luigi');
-        setAge(25);
-    }
-
-    // const handleClickAgain = (name) => {
-    //     console.log("Hello, " + name);
-    // }<button onClick={() => handleClickAgain("Mario")}>Click me</button>
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click Me!</button>
+           {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                <p>{blog.body}</p>
+            </div>
+           ))}
         </div>
      );
 }
