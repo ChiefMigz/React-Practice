05f2f7d1c6b7b0dc4f2944151190087e9924f044
ChiefMigz <pebenitomiguel@gmail.com>
2022-05-02 23:17:53 -0700

    Added communication between json databse and frontend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './Navbar';
 import Home from './Home'
 
-function App() {
+const App = () => {
 
   return (
     <div className="App">
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
-    const handleDelete = (id) =>
-    {
-        setBlogs(blogs.filter((blog) => blog.id !== id))
-    }
+    useEffect(() => {
+        setTimeout(() => { //simulate idea (don't do in real application)
+            fetch('http://localhost:8000/blogs')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+            })
+        }, 1000);
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title={'All blogs'} handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={'Mario\'s blogs'}/>
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title={'All blogs'} />}
         </div>
      );
 }
