e5497091eae283e01cb55d44a6b61c0533ad0169
ChiefMigz <pebenitomiguel@gmail.com>
2022-05-12 23:18:16 -0700

    Added redirects, blog deletetion and post and not found landing page

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+    const {data: blogs, isPending, error} = useFetch('http://192.168.0.43:8000/blogs');
     return ( 
         <div className="home">
             {isPending && <div>Loading...</div>}
